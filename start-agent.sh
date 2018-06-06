74bfde64e9fc7513ee7ac219f59bf83a7dbcbcbe
cyif <chenyifan3x@gmail.com>
2018-06-06 19:51:41 +0800

    ...

--- start-agent.sh
+++ start-agent.sh
@@ -46,7 +46,7 @@ elif [[ "$1" == "provider-large" ]]; then
        -Xms2560M \
        -Xmx2560M \
        -Dtype=provider \
-       -Dweight=3 \
+       -Dweight=4 \
        -Ddubbo.protocol.port=20880 \
        -Dserver.port=30000 \
        -Detcd.url=$ETCD_URL \
