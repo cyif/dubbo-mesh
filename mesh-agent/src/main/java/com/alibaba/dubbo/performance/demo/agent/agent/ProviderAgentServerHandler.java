8df4b3ea108bea11baaa2690712e9196c229178f
cyif <chenyifan3x@gmail.com>
2018-06-07 22:55:19 +0800

    ...

--- mesh-agent/src/main/java/com/alibaba/dubbo/performance/demo/agent/agent/ProviderAgentServerHandler.java
+++ mesh-agent/src/main/java/com/alibaba/dubbo/performance/demo/agent/agent/ProviderAgentServerHandler.java
@@ -5,6 +5,10 @@ import io.netty.channel.Channel;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Created with IntelliJ IDEA.
@@ -14,6 +18,10 @@ import io.netty.channel.ChannelInboundHandlerAdapter;
  * Time: 下午3:38
  */
 public class ProviderAgentServerHandler extends ChannelInboundHandlerAdapter {
+
+    private Logger logger = LoggerFactory.getLogger(ProviderAgentServerHandler.class);
+
+    private static AtomicInteger channelId = new AtomicInteger();
 
     private ProviderRpcClient client;
     private Channel targetChannel;
@@ -29,6 +37,7 @@ public class ProviderAgentServerHandler extends ChannelInboundHandlerAdapter {
 
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        logger.info("Provider connections : " + channelId.incrementAndGet());
         client.connect(ctx.channel()).addListener(future -> {
             targetChannel = ((ChannelFuture) future).channel();
         });
