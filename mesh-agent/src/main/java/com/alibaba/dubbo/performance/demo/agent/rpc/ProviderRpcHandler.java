aa1a0819b47e5ae6500e17f223b3544411fe5817
cyif <chenyifan3x@gmail.com>
2018-06-10 13:47:18 +0800

    ...

--- mesh-agent/src/main/java/com/alibaba/dubbo/performance/demo/agent/rpc/ProviderRpcHandler.java
+++ mesh-agent/src/main/java/com/alibaba/dubbo/performance/demo/agent/rpc/ProviderRpcHandler.java
@@ -6,6 +6,8 @@ import com.google.protobuf.ByteString;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Created with IntelliJ IDEA.
@@ -15,6 +17,8 @@ import io.netty.channel.ChannelInboundHandlerAdapter;
  * Time: 下午10:00
  */
 public class ProviderRpcHandler extends ChannelInboundHandlerAdapter {
+
+    private Logger logger = LoggerFactory.getLogger(ProviderRpcHandler.class);
 
     private Channel sourceChannel;
 
@@ -30,6 +34,7 @@ public class ProviderRpcHandler extends ChannelInboundHandlerAdapter {
                 .setId(response.getRequestId())
                 .setValueBytes(ByteString.copyFrom(response.getBytes()))
                 .build();
+        logger.info("Return value : " + agentResponse.getValue());
         sourceChannel.writeAndFlush(agentResponse);
     }
 }
