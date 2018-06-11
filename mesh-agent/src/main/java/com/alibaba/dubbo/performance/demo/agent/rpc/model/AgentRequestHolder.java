8d3770444818a30a38c96bfadbed9092c039b9d7
cyif <chenyifan3x@gmail.com>
2018-06-11 14:03:40 +0800

    ...

--- mesh-agent/src/main/java/com/alibaba/dubbo/performance/demo/agent/rpc/model/AgentRequestHolder.java
+++ mesh-agent/src/main/java/com/alibaba/dubbo/performance/demo/agent/rpc/model/AgentRequestHolder.java
@@ -4,9 +4,11 @@ import com.alibaba.dubbo.performance.demo.agent.proto.Agent;
 import io.netty.util.collection.LongObjectHashMap;
 import io.netty.util.concurrent.Promise;
 
+import java.util.concurrent.ConcurrentHashMap;
+
 public class AgentRequestHolder {
 
-    private static LongObjectHashMap<Promise<Agent.AgentResponse>> processingRpc = new LongObjectHashMap<>();
+    private static ConcurrentHashMap<Long, Promise<Agent.AgentResponse>> processingRpc = new ConcurrentHashMap<>();
 
     public static void put(long requestId, Promise<Agent.AgentResponse> promise){
         processingRpc.put(requestId, promise);
