628f1f8a71b25d49f747e6243c705a745c3113e6
cyif <chenyifan3x@gmail.com>
2018-06-11 15:53:02 +0800

    ...

--- mesh-agent/src/main/java/com/alibaba/dubbo/performance/demo/agent/rpc/model/AgentRequestHolder.java
+++ mesh-agent/src/main/java/com/alibaba/dubbo/performance/demo/agent/rpc/model/AgentRequestHolder.java
@@ -2,23 +2,28 @@ package com.alibaba.dubbo.performance.demo.agent.rpc.model;
 
 import com.alibaba.dubbo.performance.demo.agent.proto.Agent;
 import io.netty.util.collection.LongObjectHashMap;
+import io.netty.util.concurrent.FastThreadLocal;
 import io.netty.util.concurrent.Promise;
-
-import java.util.concurrent.ConcurrentHashMap;
 
 public class AgentRequestHolder {
 
-    private static ConcurrentHashMap<Long, Promise<Agent.AgentResponse>> processingRpc = new ConcurrentHashMap<>();
+    private static FastThreadLocal<LongObjectHashMap<Promise<Agent.AgentResponse>>> processingRpc =
+            new FastThreadLocal<LongObjectHashMap<Promise<Agent.AgentResponse>>>() {
+                @Override
+                protected LongObjectHashMap<Promise<Agent.AgentResponse>> initialValue() throws Exception {
+                    return new LongObjectHashMap<>();
+                }
+            };
 
     public static void put(long requestId, Promise<Agent.AgentResponse> promise){
-        processingRpc.put(requestId, promise);
+        processingRpc.get().put(requestId, promise);
     }
 
     public static Promise<Agent.AgentResponse> get(long requestId){
-        return processingRpc.get(requestId);
+        return processingRpc.get().get(requestId);
     }
 
     public static Promise<Agent.AgentResponse> remove(long requestId){
-        return processingRpc.remove(requestId);
+        return processingRpc.get().remove(requestId);
     }
 }
